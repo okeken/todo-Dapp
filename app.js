bda4b2bf967656b7e8edfff1f05ff5a5206788bc
Jesserc <raymondjesse713@gmail.com>
2022-09-03 20:41:19 +0100

    interact with todo contract

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 import getContract from "./utils/getContract.js";
+import abi from "../abi.js";
+import { CONTRACT_ADDRESS } from "../constant.js";
 
+var signer;
 async function getTodoList() {
   const contract = getContract();
+  // console.log(contract);
   try {
     const response = await contract.getTodos();
     const formatted = response.map((item) => {
@@ -17,7 +21,6 @@ async function getTodoList() {
   }
 }
 
-
 const upadateTodoUI = async () => {
   const data = await getTodoList();
   console.log(data, "data");
@@ -30,3 +33,44 @@ const upadateTodoUI = async () => {
 upadateTodoUI();
 
 // add new list
+
+//connect wallet
+const connectWallet = async () => {
+  try {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send("eth_requestAccounts", []);
+    const Signer = provider.getSigner();
+    signer = Signer;
+    const contract = getContract(Signer, CONTRACT_ADDRESS, abi);
+    // console.log(contract);
+    console.log("wallet connected");
+
+    return contract;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const addTodo = async (title, description, e) => {
+  try {
+    // e.preventDefault();
+    connectWallet();
+  
+    const contract = getContract(true);
+    console.log(contract);
+    await contract.createTodo(title, description);
+
+    return contract;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+//event listeners
+// const addTodos = document.getElementById("addTodos");
+// addTodos.addEventListener("click", addTodo);
+const field = document.getElementById("formList");
+field.addEventListener("submit", (e) => {
+  e.preventDefault();
+  addTodo(formList.input.value, formList.input.value);
+});
